55d3ca5537fd12086ac92e678d26d7c69c3f0a99
chinagliadev <chinagliavictor47@gmail.com>
2026-08-17 20:34:17 -0300

    Adiciona a entidade categoria e seus modificadores de acesso, e adiciona hashcode e equals

---
+++ src/main/java/dev/chinaglia/control_finance/entitdades/Categoria.java
@@ -0,0 +1,62 @@
+package dev.chinaglia.control_finance.entitdades;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Boolean status = true;
+	
+	public Categoria() {}
+	
+	public Categoria(Long id, String nome, Boolean status) {
+		this.id = id;
+		this.nome = nome;
+		this.status = status;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
