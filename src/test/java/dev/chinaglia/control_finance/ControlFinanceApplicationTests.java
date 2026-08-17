b9e3679d9957d388095b7ee98e41a9557e76d283
chinagliadev <chinagliavictor47@gmail.com>
2026-08-17 20:22:42 -0300

    Inicializando projeto control-finance

---
+++ src/test/java/dev/chinaglia/control_finance/ControlFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chinaglia.control_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
