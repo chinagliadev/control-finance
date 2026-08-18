998eb0ec561891caa99e9598cd989ef221e0a898
chinagliadev <chinagliavictor47@gmail.com>
2026-08-17 21:23:53 -0300

    Adiciona rota para listar usuario e testar rota do getMapping

---
+++ src/main/java/dev/chinaglia/control_finance/controllers/UsuarioController.java
@@ -0,0 +1,17 @@
+package dev.chinaglia.control_finance.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@GetMapping
+	public String getUsuario()
+	{
+	    return "rota usuario funcionando";
+	}
+	
+}
